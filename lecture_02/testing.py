4ccaf188bba49fe8c77542a8682d46b4bccfdd99
Mariano Majano Amaya <mariano@bu.edu>
2024-02-06 12:51:29 -0500

    Worksheet

--- lecture_02/testing.py
+++ lecture_02/testing.py
@@ -1,19 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
-# Load the data
 df = pd.read_csv('train.csv')
 
-# Replace all missing ages with the average age
-df['Age'] = df['Age'].fillna(df['Age'].mean())
+num_rows_with_empty = df.isnull().any(axis=1).sum()
 
-# Grouping by 'Survived' and calculating the average age
-avg_age_by_survival = df.groupby('Survived')['Age'].mean()
-
-# Plotting the bar chart
-avg_age_by_survival.plot(kind='bar', color=['red', 'green'])
-plt.xlabel('Survived')
-plt.ylabel('Average Age')
-plt.title('Average Age of Passengers by Survival Status')
-plt.xticks(ticks=[0, 1], labels=['Did Not Survive', 'Survived'], rotation=0)
-plt.show()
+print("There are " +  str(num_rows_with_empty) + " rows with at least one empty value")
